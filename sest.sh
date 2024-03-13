f6318fe30a26cd25a5badb3d0c8f80924bb0e404
Soumyajit Biswas <biswas1.soumyajit@gmail.com>
2024-03-13 07:58:06 +0530

    Initial commit

---
+++ sest.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Perform a ping scan to discover hosts and store the output in a variable
+ping_scan_output=$(sudo nmap -sn "192.168.16.0/24")
+
+# Create an empty array to store IP, MAC address, and manufacturer
+declare -a ip_mac_manufacturer
+
+# Populate the array with IP, MAC address, and manufacturer
+while IFS= read -r line; do
+    if [[ $line =~ ^Nmap ]]; then
+        ip_address=$(echo $line | awk '{print $5}')
+    elif [[ $line =~ ^MAC ]]; then
+        mac_address=$(echo $line | awk '{print $3}')
+        manufacturer=$(echo $line | awk -F '(' '{print $2}' | awk -F ')' '{print $1}')
+        ip_mac_manufacturer+=("$ip_address $mac_address $manufacturer")
+    fi
+done < <(echo "$ping_scan_output")
+
+# Iterate over each IP, MAC address, manufacturer, os and hostname
+for ip_mac in "${ip_mac_manufacturer[@]}"; do
+    # Print the IP, MAC address, and manufacturer
+    ip_address=$(echo $ip_mac | awk '{print $1}')
+    nmap_scan=$(sudo nmap -O "$ip_address" | grep "Nmap scan report" -A 20 | grep -v "Nmap scan report")
+    os_details=$(echo "$nmap_scan" | grep "OS details" | awk -F ':' '{print $NF}')
+    hostname=$(sudo nslookup "$ip_address" | awk '/name =/{print substr($4,1,length($4)-1)}')
+    echo "IP Address: $(echo $ip_mac | awk '{print $1}')"
+    echo "MAC Address: $(echo $ip_mac | awk '{print $2}')"
+    echo "Manufacturer: $(echo $ip_mac | awk '{print $3}')"
+    echo "OS Details: $os_details"
+    echo "Hostname: $hostname"
+    echo
+    echo "--------------------------------------------------------------------------------"
+    echo
+done
